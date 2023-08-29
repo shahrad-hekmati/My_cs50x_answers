fd233ba0190de38cc709ef0056358ec60b893306
Shahrad Hekmati Farid <55769279+shahrad-hekmati@users.noreply.github.com>
2023-08-29 23:57:15 +0330

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string person_name = get_string("What is your name : "); // Enter your name here
+    printf("hello, %s\n", person_name); // it will print your name in this format ==> hello, name
+
+}
